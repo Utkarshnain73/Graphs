6883438a1c13cd6dc4f301f68db156ce493c4d16
Spartenking56 <136215109+Spartenking56@users.noreply.github.com>
2024-11-10 01:20:00 -0700

    Add project files.

---
+++ WindowsFormsApp1/Mainform.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PieChartWithLegendApp
+{
+    partial class MainForm
+    {
+        private System.Windows.Forms.TextBox txtSliceLabel;
+        private System.Windows.Forms.TextBox txtSliceValue;
+        private System.Windows.Forms.Button btnSelectColor;
+        private System.Windows.Forms.Button btnAddSlice;
+        private System.Windows.Forms.Button btnDrawPieChart;
+        private System.Windows.Forms.ListBox lstSlices;
+        private System.Windows.Forms.Panel chartPanel;
+
+        private void InitializeComponent()
+        {
+            this.txtSliceLabel = new System.Windows.Forms.TextBox();
+            this.txtSliceValue = new System.Windows.Forms.TextBox();
+            this.btnSelectColor = new System.Windows.Forms.Button();
+            this.btnAddSlice = new System.Windows.Forms.Button();
+            this.btnDrawPieChart = new System.Windows.Forms.Button();
+            this.lstSlices = new System.Windows.Forms.ListBox();
+            this.chartPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtSliceLabel
+            // 
+            this.txtSliceLabel.Location = new System.Drawing.Point(20, 20);
+            this.txtSliceLabel.Name = "txtSliceLabel";
+            this.txtSliceLabel.Size = new System.Drawing.Size(100, 22);
+            this.txtSliceLabel.TabIndex = 0;
+            // 
+            // txtSliceValue
+            // 
+            this.txtSliceValue.Location = new System.Drawing.Point(130, 20);
+            this.txtSliceValue.Name = "txtSliceValue";
+            this.txtSliceValue.Size = new System.Drawing.Size(50, 22);
+            this.txtSliceValue.TabIndex = 1;
+            // 
+            // btnSelectColor
+            // 
+            this.btnSelectColor.Location = new System.Drawing.Point(190, 20);
+            this.btnSelectColor.Name = "btnSelectColor";
+            this.btnSelectColor.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectColor.TabIndex = 2;
+            this.btnSelectColor.Text = "Select Color";
+            this.btnSelectColor.Click += new System.EventHandler(this.btnSelectColor_Click);
+            // 
+            // btnAddSlice
+            // 
+            this.btnAddSlice.Location = new System.Drawing.Point(280, 20);
+            this.btnAddSlice.Name = "btnAddSlice";
+            this.btnAddSlice.Size = new System.Drawing.Size(75, 23);
+            this.btnAddSlice.TabIndex = 3;
+            this.btnAddSlice.Text = "Add Slice";
+            this.btnAddSlice.Click += new System.EventHandler(this.btnAddSlice_Click);
+            // 
+            // btnDrawPieChart
+            // 
+            this.btnDrawPieChart.Location = new System.Drawing.Point(20, 60);
+            this.btnDrawPieChart.Name = "btnDrawPieChart";
+            this.btnDrawPieChart.Size = new System.Drawing.Size(100, 23);
+            this.btnDrawPieChart.TabIndex = 4;
+            this.btnDrawPieChart.Text = "Draw Pie Chart";
+            this.btnDrawPieChart.Click += new System.EventHandler(this.btnDrawPieChart_Click);
+            // 
+            // lstSlices
+            // 
+            this.lstSlices.ItemHeight = 16;
+            this.lstSlices.Location = new System.Drawing.Point(20, 100);
+            this.lstSlices.Name = "lstSlices";
+            this.lstSlices.Size = new System.Drawing.Size(335, 84);
+            this.lstSlices.TabIndex = 5;
+            // 
+            // chartPanel
+            // 
+            this.chartPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.chartPanel.Location = new System.Drawing.Point(370, 20);
+            this.chartPanel.Name = "chartPanel";
+            this.chartPanel.Size = new System.Drawing.Size(400, 300);
+            this.chartPanel.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSliceLabel);
+            this.Controls.Add(this.txtSliceValue);
+            this.Controls.Add(this.btnSelectColor);
+            this.Controls.Add(this.btnAddSlice);
+            this.Controls.Add(this.btnDrawPieChart);
+            this.Controls.Add(this.lstSlices);
+            this.Controls.Add(this.chartPanel);
+            this.Name = "MainForm";
+            this.Text = "Pie Chart with Legend";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Mainform.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PieChartWithLegendApp
+{
+    public partial class MainForm : Form
+    {
+        private List<PieSlice> slices = new List<PieSlice>();
+        private Color currentColor = Color.Gray;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void btnAddSlice_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSliceLabel.Text) ||
+                !float.TryParse(txtSliceValue.Text, out float value))
+            {
+                MessageBox.Show("Please enter a valid label and value.");
+                return;
+            }
+
+            
+            PieSlice newSlice = new PieSlice(txtSliceLabel.Text, value, currentColor);
+            slices.Add(newSlice);
+            lstSlices.Items.Add($"{newSlice.Label}: {newSlice.Value}");
+
+            
+            txtSliceLabel.Clear();
+            txtSliceValue.Clear();
+            currentColor = Color.Gray;
+        }
+
+        private void btnDrawPieChart_Click(object sender, EventArgs e)
+        {
+            DrawPieChart(slices);
+        }
+
+        private void DrawPieChart(List<PieSlice> slices)
+        {
+            if (chartPanel == null || slices.Count == 0) return;
+
+            float totalValue = slices.Sum(s => s.Value);
+            float startAngle = 0f;
+            int legendX = chartPanel.Width - 100;
+            int legendY = 20;
+            int legendHeight = 20;
+
+            using (Graphics g = chartPanel.CreateGraphics())
+            {
+                g.Clear(Color.White);
+
+                foreach (var slice in slices)
+                {
+                    float sweepAngle = (slice.Value / totalValue) * 360;
+                    using (Brush brush = new SolidBrush(slice.Color))
+                    {
+                        
+                        g.FillPie(brush, 20, 20, chartPanel.Width - 150, chartPanel.Height - 50, startAngle, sweepAngle);
+
+                        
+                        g.FillRectangle(brush, legendX, legendY, legendHeight, legendHeight);
+                        g.DrawString(slice.Label, this.Font, Brushes.Black, legendX + legendHeight + 5, legendY);
+                        legendY += legendHeight + 5;
+                    }
+
+                    startAngle += sweepAngle;
+                }
+            }
+        }
+    }
+
+    public class PieSlice
+    {
+        public string Label { get; }
+        public float Value { get; }
+        public Color Color { get; }
+
+        public PieSlice(string label, float value, Color color)
+        {
+            Label = label;
+            Value = value;
+            Color = color;
+        }
+    }
+}
